f2f81dd06c2db07120eb2ac0b1e41e4d6d6ebe1d
devRobertSon <115036741+devRobertSon@users.noreply.github.com>
2026-08-27 17:38:53 +0900

    chore: connect Firebase web app

--- src/config.js
+++ src/config.js
@@ -3,12 +3,12 @@ export const appConfig = {
   payrollUrl: "https://payroll.robertson.kr/",
   portalUrl: "https://academy.robertson.kr/",
   firebase: {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
+    apiKey: "AIzaSyDd7tq1_xHseFTQrT-BOjCK8wTmAlGRlxk",
+    authDomain: "academy-operations-hub.firebaseapp.com",
+    projectId: "academy-operations-hub",
+    storageBucket: "academy-operations-hub.firebasestorage.app",
+    messagingSenderId: "300551162748",
+    appId: "1:300551162748:web:6218485fe010958190b2f9",
     appCheckSiteKey: ""
   }
 };
