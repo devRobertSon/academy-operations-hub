93e0e1cb2f2524ff87878f1859d6149e37b2f1fe
devRobertSon <115036741+devRobertSon@users.noreply.github.com>
2026-08-27 18:01:47 +0900

    fix: refresh Firebase config cache

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import { appConfig, isFirebaseConfigured } from "./config.js";
+import { appConfig, isFirebaseConfigured } from "./config.js?v=20260827-1";
 import {
   demoAssessmentResults,
   demoClasses,
